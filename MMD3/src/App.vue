1bdd254b7d34c9ea4ce44ecc41eb466b21fc8c99
k-worsoee-ucn <10461960@ucn.dk>
2024-01-05 17:19:36 +0100

    Started on webapp
    Splash screen functional
    Started on burger menu

---
+++ MMD3/src/App.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import TheSplashScreen from './components/TheSplashScreen.vue';
+import { ref } from 'vue';
+
+const headerVis = ref()
+</script>
+
+<template>
+  <header class="hiddenElement">
+      <nav>
+        <img class="navLogo" src="./assets/logo_laerkelundenx.svg" alt="">
+        <RouterLink to="/">Home</RouterLink>
+        <hr>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+
+  <TheSplashScreen></TheSplashScreen>
+</template>
+
+<style scoped>
+.hiddenElement {
+  display: none;
+}
+
+header {
+ grid-column: 1/span 14;
+}
+
+.navLogo {
+  width: 60%;
+  margin: 10% auto;
+}
+
+nav a {
+  margin: auto;
+}
+
+nav hr {
+  width: 50%;
+}
+
+body {
+  margin: 0;
+}
+
+nav {
+  display: grid;
+}
+
+</style>
+
+<!-- Global -->
+<style>
+  #app {
+    background-color: #E5F4FF;
+    display: grid;
+    grid-template-columns: repeat(14, 1fr);
+  }
+
+  body {
+    margin: 0;
+  }
+
+  @keyframes splash {
+  from {scale: 1;}
+  to {scale: 0; display: none;}
+}
+</style>
---
+++ MMD3/src/components/TheSplashScreen.vue
@@ -0,0 +1,35 @@
+<script setup>
+const splashLogo = document.querySelector("#splashLogo")
+const header = document.querySelector("header")
+
+    function changeVis() {
+        header.classList.remove("hiddenElement")
+    }
+
+</script>
+
+<template>
+    <div>
+        <img id="splashLogo" src="../assets/logo_laerkelundenx.svg" alt="" @animationend="changeVis">
+    </div>
+</template>
+
+<style scoped>
+div {
+    height: 100vh;
+    grid-column: 1/span 14;
+}
+
+
+</style>
+<style>
+#splashLogo {
+    width: 90%;
+    margin: 40vh 5%;
+    animation: .5s ease-out;
+    animation-name: splash;
+    animation-fill-mode: forwards;
+    animation-delay: 1s;
+}
+
+</style>
