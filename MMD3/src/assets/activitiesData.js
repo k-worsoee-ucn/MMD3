b9d82eca228af711aa0b701b33a576e34962b74e
k-worsoee-ucn <10461960@ucn.dk>
2024-01-10 23:57:29 +0100

    Update 1.1.2
    Fixed image urls

--- MMD3/src/assets/activitiesData.js
+++ MMD3/src/assets/activitiesData.js
@@ -2,50 +2,50 @@ const data = [
     {
         id: 1,
         name: "FODBOLD",
-        imgUrl: "/images/Sociale_aktiviteter/Fodbold.png",
+        imgUrl: "images/Sociale_aktiviteter/Fodbold.png",
         time: "24. Juli - 13:00",
         creator: {
-            creatorImageUrl: "/images/Personer/Martin.png",
+            creatorImageUrl: "images/Personer/Martin.png",
             creatorName: "Martin Lystrup Jensen"
         },
         desc: "Ingen fodboldstøvler? Ingen problem! Kom og vær med til en afslappet og sjov fodboldkamp kl. 17 på campingpladsens fodboldbane. Alle aldre er velkomne, det vigtigste er bare at have det sjovt.",
-        mapUrl: "/images/Maps/map.png"
+        mapUrl: "images/Maps/map.png"
     },
     {
         id: 2,
         name: "AFTEN GÅTUR",
-        imgUrl: "/images/Sociale_aktiviteter/Aften_gaatur.png",
+        imgUrl: "images/Sociale_aktiviteter/Aften_gaatur.png",
         time: "24. Juli - 19:00",
         creator: {
-            creatorImageUrl: "/images/Personer/Anna.png",
+            creatorImageUrl: "images/Personer/Anna.png",
             creatorName: "Anna Kristine Olsen"
         },
         desc: "Der vil være aften gåtur, for dem der vil med kl 19, hvor vi går en lille tur i naturen. Hunde og børn er også meget velkomne. Vi tænker at spise en is fra kiosken, når vi er tilbage på pladsen.",
-        mapUrl: "/images/Maps/map.png"
+        mapUrl: "images/Maps/map.png"
     },
     {
         id: 3,
         name: "KRABBEFISKERI",
-        imgUrl: "/images/Sociale_aktiviteter/Krabbefiskeri.png",
+        imgUrl: "images/Sociale_aktiviteter/Krabbefiskeri.png",
         time: "24. Juli - 14:00",
         creator: {
-            creatorImageUrl: "/images/Personer/Jesper.png",
+            creatorImageUrl: "images/Personer/Jesper.png",
             creatorName: "Jesper Lund Pedersen"
         },
         desc: "Kom og vær med til sjovt krabbefiskeri for børnene kl. 14 ved badebroen på campingpladsen! Tag hele familien med og nyd en hyggelig eftermiddag ved vandet. Vi har alt det nødvendige udstyr, så alle kan deltage. Vi ses der!",
-        mapUrl: "/images/Maps/map.png"
+        mapUrl: "images/Maps/map.png"
     },
     {
         id: 4,
         name: "GRILLE SKUMFIDUSER",
-        imgUrl: "/images/Sociale_aktiviteter/Skumfiduser.png",
+        imgUrl: "images/Sociale_aktiviteter/Skumfiduser.png",
         time: "24. Juli - 20:00",
         creator: {
-            creatorImageUrl: "/images/Personer/Julie.png",
+            creatorImageUrl: "images/Personer/Julie.png",
             creatorName: "Julie Maj Næstrup"
         },
         desc: "Så skal der grilles skumfiduser! Vi har købt ind så børnene kan få grillet skumfiduser efter de har spist ca kl 19 hos plads 159. Kom hid, der er massere til alle!",
-        mapUrl: "/images/Maps/map.png"
+        mapUrl: "images/Maps/map.png"
     },
     
 ]
